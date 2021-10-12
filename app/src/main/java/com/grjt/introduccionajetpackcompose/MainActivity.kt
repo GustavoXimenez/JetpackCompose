861e8ac0c4e56fa2461eed76b844452eef062aed
AzureAD\GustavoRafaelJimenez <gjimenez@biometriaaplicada.com>
2021-10-12 17:02:58 -0500

    Primeros pasos, configurando Text

--- app/src/main/java/com/grjt/introduccionajetpackcompose/MainActivity.kt
+++ app/src/main/java/com/grjt/introduccionajetpackcompose/MainActivity.kt
@@ -3,10 +3,13 @@ package com.grjt.introduccionajetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import com.grjt.introduccionajetpackcompose.ui.theme.IntroduccionAJetpackComposeTheme
 
@@ -15,24 +18,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             IntroduccionAJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                SimpleComposable()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SimpleComposable() {
+    val checkState = remember { mutableStateOf(false) }
+
+    Column {
+        Text("Hola mundo")
+        Checkbox(checked = checkState.value, onCheckedChange = {
+            checkState.value = !checkState.value
+        })
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "JetpackCompose", showSystemUi = true, device = Devices.DEFAULT)
 @Composable
 fun DefaultPreview() {
-    IntroduccionAJetpackComposeTheme {
-        Greeting("Android")
-    }
+    SimpleComposable()
 }
