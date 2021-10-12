fcc783ac0ecfcb877eb43253fbd0acedc6e1625b
AzureAD\GustavoRafaelJimenez <gjimenez@biometriaaplicada.com>
2021-10-12 17:17:21 -0500

    Row and Column en JetpackCompose

--- app/src/main/java/com/grjt/introduccionajetpackcompose/MainActivity.kt
+++ app/src/main/java/com/grjt/introduccionajetpackcompose/MainActivity.kt
@@ -3,14 +3,21 @@ package com.grjt.introduccionajetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.grjt.introduccionajetpackcompose.ui.theme.IntroduccionAJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,18 +32,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun SimpleComposable() {
-    val checkState = remember { mutableStateOf(false) }
+fun Figure(color: Color) {
+    Box(
+        modifier = Modifier
+            .width(50.dp)
+            .height(50.dp)
+            .clip(CircleShape)
+            .background(color)
+    )
+}
 
-    Column {
-        Text("Hola mundo")
-        Checkbox(checked = checkState.value, onCheckedChange = {
-            checkState.value = !checkState.value
-        })
+@Composable
+fun SimpleComposable() {
+    Column (
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Figure(color = Color.Red)
+        Figure(color = Color.Green)
+        Figure(color = Color.Blue)
     }
 }
 
-@Preview(showBackground = true, name = "JetpackCompose", showSystemUi = true, device = Devices.DEFAULT)
+@Preview(
+    showBackground = true,
+    name = "JetpackCompose",
+    showSystemUi = true,
+    device = Devices.DEFAULT
+)
 @Composable
 fun DefaultPreview() {
     SimpleComposable()
