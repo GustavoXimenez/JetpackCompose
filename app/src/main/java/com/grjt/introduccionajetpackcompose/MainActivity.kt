99b88e30ecef96de2ffa55e04f6ae042ae36be2e
AzureAD\GustavoRafaelJimenez <gjimenez@biometriaaplicada.com>
2021-10-12 21:53:19 -0500

    Diseñar componentes basicos, textos, textfields, botones e imagenes.

--- app/src/main/java/com/grjt/introduccionajetpackcompose/MainActivity.kt
+++ app/src/main/java/com/grjt/introduccionajetpackcompose/MainActivity.kt
@@ -3,21 +3,27 @@ package com.grjt.introduccionajetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
-import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.grjt.introduccionajetpackcompose.ui.theme.IntroduccionAJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,47 +39,59 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SimpleComposable() {
-    var num by rememberSaveable {
-        mutableStateOf(0)
-    }
+    var textFieldValue by remember { mutableStateOf("") }
+    var textValue by remember { mutableStateOf("Título de la aplicación") }
 
-    Column {
-        Text("Conteo: $num")
-
-        Divider(thickness = 20.dp, color = Color.Transparent)
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Counter(
-                btnTitle = "Aumentar conteo",
-                updateCount = {
-                    num++
-                }
-            )
-
-            Counter(
-                btnTitle = "Disminuir conteo",
-                updateCount = {
-                    num--
-                }
-            )
-        }
-    }
-}
-
-@Composable
-fun Counter(
-    btnTitle: String,
-    updateCount: () -> Unit
-) {
-    Button(
-        onClick = {
-            updateCount()
-        }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
-        Text(btnTitle)
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp),
+            painter = painterResource(id = R.drawable.bg),
+            contentDescription = "Imagen de fondo",
+            contentScale = ContentScale.FillWidth
+        )
+
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = textValue,
+            style = TextStyle(
+                color = Color.Green,
+                fontSize = 28.sp,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Black,
+                textAlign = TextAlign.Center
+            )
+        )
+
+        TextField(
+            value = textFieldValue,
+            onValueChange = { newText ->
+                textFieldValue = newText
+            },
+            label = {
+                Text("Inserte texto aquí")
+            },
+            leadingIcon = {
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    painter = painterResource(id = R.drawable.ic_android),
+                    contentDescription = "Icono"
+                )
+            }
+        )
+
+        Button(
+            onClick = {
+                textValue = textFieldValue
+            }
+        ) {
+            Text("Aceptar")
+        }
     }
 }
 
